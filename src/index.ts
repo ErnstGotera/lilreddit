64968c03aef3fd85c5f4017775cb72fb3294a739
programming-ataraxia <ernestogl5@hotmail.com>
2020-08-30 16:58:40 -0400

    apollo and express server setup, MikroOrm TypeGraphql crud start

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
+import 'reflect-metadata';
 import microConfig from './mikro-orm.config';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up;
 
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server started');
+  });
 };
