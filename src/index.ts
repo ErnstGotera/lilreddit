c6e7a22b4fb7d96a115f741cc2fba0f29ca97aeb
ernstgotera <ernestogl5@hotmail.com>
2021-02-17 18:35:55 -0400

    add entities and migrations to the db connection

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,24 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
-import { Post } from 'entities/Post';
-import { User } from 'entities/User'; 
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
+import { Updoot } from "./entities/Updoot";
+import path from "path";
 
 const main = async () => {
   const conn = await createConnection({
     type: 'postgres',
+    url: process.env.DATABASE_URL,
     database: 'lilreddit',
     logging: true,
     synchronize: true,
-    entities: [Post, User]
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Post, User, Updoot]
   })
 
   const app = express();
+  
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [ PostResolver, UserResolver],
